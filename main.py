c8e79c78c23656e8e64726a6ef19c1862adf2f96
Pankaj Tulaskar <pst28@njit.edu>
2019-01-24 18:58:09 -0500

    Created func to clean data

--- main.py
+++ main.py
@@ -11,4 +11,15 @@ def start(url):
         for each_word in words:
             word_list.append(each_word)
 
+        cleanup_list(word_list)
+
+def cleanup_list(word_list):
+    clean_word_list = []
+    for word in word_list:
+        symbols = "~!@$%^&*()_+`-={}|:\">?[]\;',./'"
+        for i in range(0, len(symbols)):
+            word = word.replace(symbols[i], "")
+        if len(word) > 0:
+            clean_word_list.append(word)
+
 start("https://newyork.craigslist.org/d/automotive-services/search/aos")
