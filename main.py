77e2788c87d582bc0866cee5ae7c6e8fea9b0337
Pankaj Tulaskar <pst28@njit.edu>
2019-01-24 19:06:46 -0500

    Created func to count frequency of the words

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+import operator
 
 def start(url):
     word_list = []
@@ -11,15 +12,28 @@ def start(url):
         for each_word in words:
             word_list.append(each_word)
 
-        cleanup_list(word_list)
+    cleanup_list(word_list)
 
 def cleanup_list(word_list):
     clean_word_list = []
     for word in word_list:
-        symbols = "~!@$%^&*()_+`-={}|:\">?[]\;',./'"
+        symbols = "🚧 ⚑★🚙#🚘~!@🌟✔👍$%^&*()_+`-={}|:\">?[]\;',./'"
         for i in range(0, len(symbols)):
             word = word.replace(symbols[i], "")
         if len(word) > 0:
             clean_word_list.append(word)
+
+    create_dictionary(clean_word_list)
+
+def create_dictionary(clean_word_list):
+    word_count = {}
+    for word in clean_word_list:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+    for key, value in sorted(word_count.items(), key=operator.itemgetter(1)):
+        print(key, value)
 
 start("https://newyork.craigslist.org/d/automotive-services/search/aos")
